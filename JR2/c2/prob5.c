f9a52375d4a47ca82421e41ca995ed1261a683d9
sakuyuki1341 <forestry.for@gmail.com>
2019-06-07 02:51:51 +0900

    問題5を追加

---
+++ JR2/c2/prob5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_byte(unsigned char x) {
+    for(int i = 7; i >= 0; i--) {
+        printf("%d", (x>>i) & 1);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    show_byte(0x6a);
+    show_byte(0x0f);
+    show_byte((0x6a)|(0x0f));
+    show_byte((0x6a)&(0x0f));
+    show_byte(~(0x6a));
+    return 0;
+}
