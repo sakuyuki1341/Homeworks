559eecfbde7905a906cef1de7caa9de8e305a815
sakuyuki1341 <forestry.for@gmail.com>
2019-06-07 03:17:07 +0900

    宿題の問題2の追加

---
+++ JR2/c2/prob_hw2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_byte(unsigned char x) {
+    for(int i = 7; i >= 0; i--) {
+        printf("%d", (x>>i) & 1);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    show_byte(((0x6a)>>4)<<4);
+    return 0;
+}
