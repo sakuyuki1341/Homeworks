5929716651d553478c3b2746d483f3145cb8eae0
sakuyuki1341 <forestry.for@gmail.com>
2019-07-02 14:25:51 +0900

    hogehoge

--- ProTU/11/mergesort1_demo.c
+++ ProTU/11/mergesort1_demo.c
@@ -9,8 +9,9 @@
 #define MAXDATA 1000000
 
 // #define ENSHU_EX
- #define ENSHU_4_FT
+// #define ENSHU_4_FT
 // #define ENSHU_4_ST
+ #define ENSHU_4_TT
 
 static int a[MAXDATA];
 
@@ -45,6 +46,29 @@ void qs(int *a, int i, int j) {
 }
 
 void quicksort(int *a, int n) { qs(a, 0, n-1); }
+
+// quicksort_adv-------------------------------------------
+int med (int x, int y, int z) {
+    if(a[x] > a[y]) {
+        if(a[y] > a[z]){ return y; }
+        else if(a[z] > a[x]) { return x; }
+        else { return z; }
+    } else {
+        if(a[x] > a[z]) { return x; }
+        else if(a[z] > a[y]) { return y; }
+        else { return z; }
+    }
+}
+void qs_adv(int* a, int i, int j) {
+    if(j <= i) { return; }
+    int s = i, pivot = a[med(i, (i+j)/2, j)];
+    for(int k = i; k < j; ++k) {
+      if(a[k] < pivot) { iswap(a, s++, k); }
+    }
+    iswap(a, j, s); qs(a, i, s-1); qs(a, s+1, j);
+  }
+
+ void quicksort_adv(int *a, int n) { qs_adv(a, 0, n-1); }
 
 // use_queue-----------------------------------------------
 void mergesort1(int *a, int n) {
@@ -97,6 +121,22 @@ int main(int argc, char *argv[]) {
   for(int i = 0; i < n; ++i) { a[i] = i; }
   clock_gettime(CLOCK_REALTIME, &tm1);
   quicksort(a, n);
+  clock_gettime(CLOCK_REALTIME, &tm2);
+  for(int i = 0; i < n; ++i) { printf(" %d", a[i]); }
+  printf("\n");
+  double dt = (tm2.tv_sec-tm1.tv_sec) + 1e-9*(tm2.tv_nsec-tm1.tv_nsec);
+  printf("time = %.4f\n", dt);
+  return 0;
+}
+#endif
+#ifdef ENSHU_4_TT
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+  srand(time(NULL));
+  struct timespec tm1, tm2;
+  for(int i = 0; i < n; ++i) { a[i] = i; }
+  clock_gettime(CLOCK_REALTIME, &tm1);
+  quicksort_adv(a, n);
   clock_gettime(CLOCK_REALTIME, &tm2);
   for(int i = 0; i < n; ++i) { printf(" %d", a[i]); }
   printf("\n");
