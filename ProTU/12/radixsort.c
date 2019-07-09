5b6258552dfee7b2187db7a42022540657ae513d
sakuyuki1341 <forestry.for@gmail.com>
2019-07-09 20:05:30 +0900

    hoge

--- ProTU/12/radixsort.c
+++ ProTU/12/radixsort.c
@@ -2,26 +2,50 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 
 #define MAXDATA 1000000
 
-static int arr_f[MAXDATA];
+static int a[MAXDATA];
 
 static void swap(int *a, int i, int j) {
     int x = a[i]; a[i] = a[j]; a[j] = x;
 }
 
-static void rs(int *a, int i, int j, int mask) {
+static void rs1(int *a, int i, int j, int mask) {
     if(i >= j || mask == 0) { return; }
     int k, s;
     for(s = k = i; k <= j; ++k) {
         if((a[k]&mask) == 0) { swap(a, s++, k); }
     }
-    rs(a, i, s-1, mask/2); rs(a, s, j, mask/2);
+    rs1(a, i, s-1, mask/2); rs1(a, s, j, mask/2);
 }
 
-void radixsort(int n, int *a) { rs(a, 0, n-1, 0x40000000); }
+static void rs2(int *a, int n) {
+    int maxmask = MAXDATA;
+    int *b = (int*)(malloc(sizeof(int)*n)); int bc = 0;
+    int *c = (int*)(malloc(sizeof(int)*n)); int cc = 0;
+    for(int mask = 1; mask < maxmask; mask *= 2) {
+        for(int i = 0; i < n; ++i) {
+            if((a[i]&mask) == 0) {
+                b[bc++] = a[i];
+            } else {
+                c[cc++] = a[i];
+            }
+        }
+        int ac = 0;
+        for(int i = 0; i < bc+1; i++) {
+            a[ac++] = b[bc++];
+        }
+        for(int i = 0; i < cc+1; i++) {
+            a[ac++] = c[cc++];
+        }
+    }
+}
+
+void radixsort1(int n, int *a) { rs1(a, 0, n-1, 0x40000000); }
+
+void radixsort2(int n, int *a) { rs2(a, n); }
 
 int makearr();
 
@@ -35,12 +59,12 @@ int main(int argc, char const *argv[]) {
     srand(time(NULL));
     struct timespec tm1, tm2;
 
-    for(int i = 0; i < n; ++i) { arr_f[i] = rand()%10000; }
+    for(int i = 0; i < n; ++i) { a[i] = rand()%10000; }
 
     clock_gettime(CLOCK_REALTIME, &tm1);
-    radixsort(n, arr_f);
+    radixsort1(n, a);
     clock_gettime(CLOCK_REALTIME, &tm2);
-    for(int i = 0; i < n; ++i) { printf(" %d", arr_f[i]); }
+    for(int i = 0; i < n; ++i) { printf(" %d", a[i]); }
     printf("\n");
     double dt = (tm2.tv_sec-tm1.tv_sec) + 1e-9*(tm2.tv_nsec-tm1.tv_nsec);
     printf("time = %.4f\n", dt);
