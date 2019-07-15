7dcda9d566aa08242d96a9ab53a1f5b0aba8977b
sakuyuki <forestry.for@gmail.com>
2019-07-15 17:02:45 +0900

    hoge

--- ProTU/11/bogosort.c
+++ ProTU/11/bogosort.c
@@ -4,10 +4,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define MAXDATA 1000000
-
-static int a[MAXDATA];
-
 static void swap(int *a, int i, int j) {
     int x = a[i]; a[i] = a[j]; a[j] = x;
 }
@@ -34,6 +30,26 @@ void bogosort(int* a, int n) {
     bs(a, n);
 }
 
+void expect_sort_iarray(int n, void (*f)(int m, int *a), char *msg) {
+  int c = 0, *a = (int*)malloc(n * sizeof(int));
+  srand(time(NULL));
+  for(int i = 0; i < n; ++i) { a[i] = rand()%10000; }
+  struct timespec tm1, tm2;
+  clock_gettime(CLOCK_REALTIME, &tm1);
+  (*f)(n, a);
+  clock_gettime(CLOCK_REALTIME, &tm2);
+  for(int i = 1; i < n; ++i) {
+    if(a[i-1] <= a[i]) { continue; } // correct order
+    if(++c < 5) {
+      printf("  wrong order at %d: %d > %d\n", i-1, a[i-1], a[i]);
+    } else if(c == 5) {
+      printf("  more wrong place omitted.\n");
+    }
+  }
+  double dt = (tm2.tv_sec-tm1.tv_sec) + 1e-9*(tm2.tv_nsec-tm1.tv_nsec);
+  printf("%s time=%.4f %s\n", c==0?"OK":"NG", dt, msg); free(a);
+}
+
 int main(int argc, char const *argv[]) {
     if(!(argc == 2 && argv[1] > 0)) {
         printf("並び替える数の量を指定してください\n");
@@ -41,17 +57,6 @@ int main(int argc, char const *argv[]) {
     }
 
     int n = atoi(argv[1]);
-    srand(time(NULL));
-    struct timespec tm1, tm2;
-
-    for(int i = 0; i < n; ++i) { a[i] = rand()%10000; }
-
-    clock_gettime(CLOCK_REALTIME, &tm1);
-    bogosort(a, n);
-    clock_gettime(CLOCK_REALTIME, &tm2);
-    for(int i = 0; i < n; ++i) { printf(" %d", a[i]); }
-    printf("\n");
-    double dt = (tm2.tv_sec-tm1.tv_sec) + 1e-9*(tm2.tv_nsec-tm1.tv_nsec);
-    printf("time = %.4f\n", dt);
+    expect_sort_iarray(n, bogosort, "bogosort");
     return 0;
 }
