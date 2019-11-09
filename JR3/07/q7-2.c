9ee63edd5b2e7b2d773e4e61938e3d93993d6e8f
sakuyuki <forestry.for@gmail.com>
2019-11-10 06:15:25 +0900

    q7-5まで完了

--- JR3/07/q7-2.c
+++ JR3/07/q7-2.c
@@ -2,17 +2,11 @@
  * マクロ部
 /************************************************/
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
 
 /************************************************
  * 構造体等宣言部
 /************************************************/
-typedef struct point {	//座標構造体
-	int x;
-	int y;
-} point;
+struct point { int x, y; };	//座標構造体
 
 /************************************************
  * グローバル変数
@@ -21,37 +15,15 @@ typedef struct point {	//座標構造体
 /************************************************
  * プロトタイプ宣言部
 /************************************************/
-int compare_by(point p1, point p2, char c);
-int max_index_by(point a[], int n, char c);
 
 /************************************************
  * 関数部
 /************************************************/
 ///
-/// メイン関数
-///
-int main(int argc, char const *argv[]) {
-	char c, buf[128];	//入力保存用
-	point p, arr[128];	//入力保存用
-	int i = 0;
-	//基準取得
-	scanf("%c ", &c);
-	//座標取得
-	while(fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
-		sscanf(buf, "%d %d", &p.x, &p.y);
-		arr[i] = p;
-		i++;
-	}
-	//最大の座標の表示
-	printf("%d\n", max_index_by(arr, i, c));
-	return 0;
-}
-
-///
 /// 座標p1, p2に対し、文字cで指定された基準で比較した結果を「1, 0, -1」として返す
 /// p1がp2より大きい場合: 1 / p1とp2が等しい場合: 0 / p1がp2より小さい場合: -1
 ///
-int compare_by(point p1, point p2, char c) {
+int compare_by(struct point p1, struct point p2, char c) {
 	int ret = 0;
 	if(c == 'X') {	//基準がXの時
 		if(p1.x > p2.x) {
@@ -110,13 +82,33 @@ int compare_by(point p1, point p2, char c) {
 ///
 /// 配列aに対し、先頭からn個のうち、文字cで指定された基準で最大の要素の添字を返す
 ///
-int max_index_by(point a[], int n, char c) {
+int max_index_by(struct point a[], int n, char c) {
 	int max_place = 0;	//一時最大値の位置
 	//n個まで繰り返し
-	for(int i = 0; i < n; i++) {
+	for(int i = 1; i < n; i++) {
 		if(compare_by(a[i], a[max_place], c) >= 0) {	//一時最大値の更新
 			max_place = i;
 		}
 	}
 	return max_place;	//最大値の位置を返す
+}
+
+///
+/// メイン関数
+///
+int main(int argc, char const *argv[]) {
+	char c, buf[128];	//入力保存用
+	struct point p, arr[128];	//入力保存用
+	int i = 0;
+	//基準取得
+	scanf("%c ", &c);
+	//座標取得
+	while(fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	//最大の座標の表示
+	printf("%d\n", max_index_by(arr, i, c));
+	return 0;
 }
