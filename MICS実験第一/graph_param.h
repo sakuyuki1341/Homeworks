a07d88711de703a3bae69d30f0869c17063d82ad
sakuyuki <forestry.for@gmail.com>
2020-05-13 15:36:02 +0900

    J8開始

---
+++ MICS実験第一/graph_param.h
@@ -0,0 +1,7 @@
+#ifndef GRAPH_PARAM_H_INCLUDED
+#define GRAPH_PARAM_H_INCLUDED
+
+#define	MAXV		10000		/* maximum number of vertices */
+#define MAXDEGREE	50		/* maximum outdegree of a vertex */
+
+#endif
