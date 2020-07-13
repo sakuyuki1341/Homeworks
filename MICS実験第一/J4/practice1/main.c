ca739231271397be954cc8ddba7beee835b131bb
sakuyuki <forestry.for@gmail.com>
2020-07-14 00:39:19 +0900

    hoge

--- MICS実験第一/J4/practice1/main.c
+++ MICS実験第一/J4/practice1/main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #define BSIZE 512
+
 char buf[BSIZE];
 
 void copyfile(int fOrigin, int fFake) {
 	int n = BSIZE;
-	while (n == BSIZE) {
+	while (n != 0) {
 		n = read(fOrigin, buf, BSIZE);
+		printf("n = %d", n);
 		write(1, buf, n);
 		printf("#");
 	}
@@ -19,13 +21,15 @@ int main(int argc, char **argv) {
 		printf("incorrect argument\n");
 		exit(1);
 	}
-	fOrigin = open(argv[1],O_RDONLY|O_CREAT|O_TRUNC,0644);
-	fFake = open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,0644);
+
+	fFake = open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,0666);
+	fOrigin = open(argv[1],O_RDONLY|O_CREAT|O_TRUNC,0666);
 
 	if (fOrigin < 0) {
 		//error(argv[1]);
 		exit(1);
 	}
+	
 	if (fFake < 0) {
 		//error(argv[2]);
 		exit(1);
