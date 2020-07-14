62282891568b15184a4ec6a3a5cfb97060570904
sakuyuki <forestry.for@gmail.com>
2020-07-15 00:53:53 +0900

    hoge

--- MICS実験第一/J4/practice3/main.c
+++ MICS実験第一/J4/practice3/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #define BSIZE 1024
 
-int main(int argc, char **argv) {
+int old_main(int argc, char **argv) {
 	int outFile, pid, p_fd[2];
 	char *new_program, **new_argv, buf[BSIZE];
 	if (argc < 2) {
@@ -39,4 +39,44 @@ int main(int argc, char **argv) {
 		write(outFile, buf, n);
 		printf("%s", buf);
 	}
+	exit(0);
+}
+
+int main(int argc, char **argv) {
+	int outFile, pid, p_fd[2];
+	char *new_program, **new_argv, buf[BSIZE];
+	if (argc < 2) {
+		printf("incorrect argument\n");
+		exit(1);
+	}
+	outFile = open(argv[1],O_WRONLY|O_CREAT, 0666);
+	pipe(p_fd);
+
+	if ((pid = fork()) == 0) {
+		/* child process */
+		sleep(1);
+		int n = BSIZE;
+		while (true) {
+			n = read(p_fd[0], buf, BSIZE);
+			write(outFile, buf, n);
+			printf("%s", buf);
+		}
+	}
+
+	/* parent process */
+	if (pid == -1) {
+		perror("fork");
+		exit(1);
+	}
+
+	close(p_fd[0]);
+	close(1);
+	dup(p_fd[1]);
+	close(p_fd[1]);
+	new_program = argv[2];
+	new_argv = &argv[2];
+	execvp(new_program, new_argv);
+	exit(1);
+
+	exit(0);
 }
