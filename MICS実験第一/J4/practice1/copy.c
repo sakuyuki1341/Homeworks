608812dc187527a4b2d809cc7246625130016cb8
sakuyuki <forestry.for@gmail.com>
2020-07-14 18:10:47 +0900

    簡単なcpコマンド作成

--- MICS実験第一/J4/practice1/copy.c
+++ MICS実験第一/J4/practice1/copy.c
@@ -1,7 +1,8 @@
+/*writeとreadを使用したプログラム*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#define BSIZE 512
+#define BSIZE 4096
 
 char buf[BSIZE];
 
@@ -9,9 +10,7 @@ void copyfile(int fOrigin, int fFake) {
 	int n = BSIZE;
 	while (n != 0) {
 		n = read(fOrigin, buf, BSIZE);
-		printf("n = %d", n);
-		write(1, buf, n);
-		printf("#");
+		write(fFake, buf, n);
 	}
 }
 
@@ -22,16 +21,14 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	fFake = open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,0666);
-	fOrigin = open(argv[1],O_RDONLY|O_CREAT|O_TRUNC,0666);
+	fOrigin = open(argv[1],O_RDONLY);
+	fFake = open(argv[2],O_WRONLY|O_CREAT, 0666);
 
 	if (fOrigin < 0) {
-		//error(argv[1]);
 		exit(1);
 	}
 	
 	if (fFake < 0) {
-		//error(argv[2]);
 		exit(1);
 	}
 
---
+++ MICS実験第一/J4/practice1/stdcopy.c
@@ -0,0 +1,38 @@
+/*標準入出力ライブラリを用いたプログラム*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#define BSIZE 4096
+
+char buf[BSIZE];
+
+void copyfile(FILE *fOrigin, FILE *fFake) {
+	while ((fgets(buf, BSIZE, fOrigin)) != NULL) {
+		fputs(buf, fFake);
+	}
+}
+
+int main(int argc, char **argv) {
+	FILE *fOrigin, *fFake;
+	if (argc != 3) {
+		printf("incorrect argument\n");
+		exit(1);
+	}
+
+	fOrigin = fopen(argv[1],"r");
+	fFake = fopen(argv[2],"w");
+
+	if (fOrigin < 0) {
+		exit(1);
+	}
+	
+	if (fFake < 0) {
+		exit(1);
+	}
+
+	copyfile(fOrigin, fFake);
+
+	fclose(fOrigin);
+	fclose(fFake);
+	exit(0);
+}
