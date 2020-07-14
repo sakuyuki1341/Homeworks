246d3abf85946f92cc02a9abdf69c694c94236e2
sakuyuki <forestry.for@gmail.com>
2020-07-15 01:23:52 +0900

    hoge

--- MICS実験第一/J4/practice3/main.c
+++ MICS実験第一/J4/practice3/main.c
@@ -4,44 +4,6 @@
 #include <unistd.h>
 #define BSIZE 1024
 
-int old_main(int argc, char **argv) {
-	int outFile, pid, p_fd[2];
-	char *new_program, **new_argv, buf[BSIZE];
-	if (argc < 2) {
-		printf("incorrect argument\n");
-		exit(1);
-	}
-	outFile = open(argv[1],O_WRONLY|O_CREAT, 0666);
-	pipe(p_fd);
-
-	if ((pid = fork()) == 0) {
-		/* child process */
-		close(p_fd[0]);
-		close(1);
-		dup(p_fd[1]);
-		close(p_fd[1]);
-		new_program = argv[2];
-		new_argv = &argv[2];
-		execvp(new_program,new_argv);
-		perror(new_program);
-		exit(1);
-	}
-
-	/* parent process */
-	if (pid == -1) {
-		perror("fork");
-		exit(1);
-	}
-
-	int n = BSIZE;
-	while (n != 0) {
-		n = read(p_fd[0], buf, BSIZE);
-		write(outFile, buf, n);
-		printf("%s", buf);
-	}
-	exit(0);
-}
-
 int main(int argc, char **argv) {
 	int outFile, pid, p_fd[2];
 	char *new_program, **new_argv, buf[BSIZE];
@@ -54,12 +16,11 @@ int main(int argc, char **argv) {
 
 	if ((pid = fork()) == 0) {
 		/* child process */
-		sleep(1);
 		int n = BSIZE;
 		while (1) {
-			n = read(p_fd[0], buf, BSIZE);
+			n = read(0, buf, BSIZE);
 			write(outFile, buf, n);
-			printf("%s", buf);
+			write(p_fd[1], buf, n);
 		}
 	}
 
@@ -69,14 +30,17 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	close(p_fd[0]);
-	close(1);
-	dup(p_fd[1]);
+	if((w = wait(NULL)) == -1) {
+		perror("wait");
+		exit(1);
+	}
+
 	close(p_fd[1]);
+	close(0);
+	dup(p_fd[0]);
+	close(p_fd[0]);
+
 	new_program = argv[2];
 	new_argv = &argv[2];
 	execvp(new_program, new_argv);
-	exit(1);
-
-	exit(0);
 }
