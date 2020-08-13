8431ba3ea7a34d41ede3a108d330e31aab84cd2e
sakuyuki <forestry.for@gmail.com>
2020-08-13 23:33:41 +0900

    hoge

--- MICS実験第一/J5/LastReport/t20.c
+++ MICS実験第一/J5/LastReport/t20.c
@@ -57,6 +57,10 @@ int node(int t, int l, int r) {
 void dotree(int i) {
 int stk;
 printf(" .section .rodata\n");
+int j;
+for(j = 0; j < stabuse; j++) {
+	printf(".Lvar%d: .string\"%s\"\n", j, stab[j].name);
+}
 printf(".Lprompt: .string\"%%ld > \"\n"); /* プロンプト */
 printf(".Lread: .string\"%%ld\"\n"); /* 読み取り用書式 */
 printf(".Lprint: .string\"%%ld\\n\"\n"); /* 書き出し用書式 */
@@ -81,7 +85,7 @@ void emittree(int i)
 		emittree(ntab[i].right);
 		break;
 	case T_READ: printf(" movq $.Lprompt,%%rdi\n");
-		printf(" movq %d, %%rsi\n", stab[ntab[i].left].name);
+		printf(" movq $.Lvar%d, %%rsi\n", ntab[i].left);
 		printf(" movq $0,%%rax\n"); /* 浮動小数点レジスタを使わない */
 		printf(" call printf\n");
 		printf(" leaq %d(%%rbp),%%rsi\n", -(ntab[i].left+1)*8);
